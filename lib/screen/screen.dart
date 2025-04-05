3eb1a96ae8c5751b9d349c404d43f835a280eff8
Roque Mancilla <roquemancilla@gmail.com>
2025-04-05 04:27:12 -0300

    Clase 4 ejemplo ptt

---
+++ lib/screen/screen.dart
@@ -0,0 +1,5 @@
+export 'package:flutter_application_1/screen/detail_screen.dart';
+export 'package:flutter_application_1/screen/error_screen.dart';
+export 'package:flutter_application_1/screen/home_screen.dart';
+export 'package:flutter_application_1/screen/list_screen.dart';
+
