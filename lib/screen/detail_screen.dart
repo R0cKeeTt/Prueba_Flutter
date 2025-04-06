5f73e670a159accb8932fa3d41bb200f83084b5d
Roque Mancilla <roquemancilla@gmail.com>
2025-04-05 23:17:45 -0300

    mod en detail_screen

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -4,17 +4,13 @@ class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    final arguments = (ModalRoute.of(context)?.settings.arguments ??
-        <String, dynamic>{}) as Map;
+    final arguments =
+        (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{})
+            as Map;
     final nom = arguments['nombre'];
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(
-          255,
-          165,
-          0,
-          1,
-        ),
+        backgroundColor: const Color.fromRGBO(255, 165, 0, 1),
         title: Text(nom),
         elevation: 10,
       ),
@@ -30,12 +26,14 @@ class DetailScreen extends StatelessWidget {
               padding: EdgeInsets.all(5.0),
               child: Center(
                 child: Image(
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        'https://www.comedera.com/wp-content/uploads/2022/12/Empanada-chilena-de-pollo-shutterstock_478589707.jpg')),
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    'https://www.comedera.com/wp-content/uploads/2022/12/Empanada-chilena-de-pollo-shutterstock_478589707.jpg',
+                  ),
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
